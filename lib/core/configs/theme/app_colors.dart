72eefd3cb7dc0ec646d8c293d1d3e5cb7eb8b8ed
3mad Hany <144627820+3madhani@users.noreply.github.com>
2025-04-03 00:17:31 +0200

    Add color constants for primary, background, and secondaryBackground in AppColors

--- lib/core/configs/theme/app_colors.dart
+++ lib/core/configs/theme/app_colors.dart
@@ -1,3 +1,7 @@
+import 'package:flutter/material.dart';
+
 class AppColors {
-  sta
+  static const primary = Color(0xFFE21221);
+  static const background = Color(0xFF181A20);
+  static const secondaryBackground = Color(0xFF35383E);
 }
---
+++ lib/core/configs/theme/app_theme.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:moviezone/core/configs/theme/app_colors.dart';
+
+class AppTheme {
+  static final appTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.background,
+    brightness: Brightness.dark,
+    snackBarTheme: const SnackBarThemeData(
+      backgroundColor: AppColors.background,
+      contentTextStyle: TextStyle(color: Colors.white),
+    ),
+    inputDecorationTheme: const InputDecorationTheme(
+      filled: true,
+      fillColor: AppColors.secondaryBackground,
+      hintStyle: TextStyle(
+        color: Color(0xFFA7A7A7),
+        fontWeight: FontWeight.w400,
+      ),
+      contentPadding: EdgeInsets.all(16),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(4)),
+        borderSide: BorderSide.none,
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(4)),
+        borderSide: BorderSide.none,
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+        backgroundColor: AppColors.primary,
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+      ),
+    ),
+  );
+}
